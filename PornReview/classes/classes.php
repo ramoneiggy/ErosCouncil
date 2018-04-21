38b5dcaa29b6a3f1c5088ea1d3f001e729cec060
Vangoda <38542251+Vangoda@users.noreply.github.com>
2018-04-21 13:32:49 +0200

    Add a draw class
    This class is used to draw page elements.

---
+++ PornReview/classes/classes.php
@@ -0,0 +1,25 @@
+<?php
+
+class Draw 
+{
+    /*  We will use Draw class to draw different elements of
+    //  the page.
+    */
+
+    
+    public static function drawHeader(){
+        //CODE TO DRAW HEADER
+    }
+
+    public static function drawList(){
+        //CODE TO DRAW LIST
+    }
+
+    public static function drawLoginForm(){
+        //CODE TO DRAW LOGIN OVERLAY
+    }
+
+    public static function drawRegistrationForm(){
+        //CODE TO DRAW REGISTRATION OVERLAY
+    }
+}
