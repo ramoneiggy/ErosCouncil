c048f4826497b510993adf2dc552cc390729a617
ramoneiggy <ramoneiggy@gmail.com>
2018-05-09 23:25:04 +0200

    small fix for userInfo

--- classes.php
+++ classes.php
@@ -208,7 +208,7 @@ class Draw
         echo "
             <div class='user-info col-sm-12'>
                 <div class='row'>
-                    <div class='col-sm-6'>
+                    <div class='col-sm-4'>
                         <img class='avatar' src='".$userInfo['avatar']."' alt='Avatar'>
                     </div>
                     ";
@@ -217,7 +217,7 @@ class Draw
                     }
                 echo " 
                     
-                    <div class='col-sm-6'>
+                    <div class='col-sm-8'>
                         <form action='submitAvatar.php' method='post' enctype='multipart/form-data'>
                         <b>Change avatar:</b>
                             <input class='btn' type='file' name='avatar' id='avatar'>
